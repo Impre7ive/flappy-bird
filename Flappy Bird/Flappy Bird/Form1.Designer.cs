4708d5883ac9f80104cd4460416bc60cec49216a
Dmitry Liashenko <adslyashenko@gmail.com>
2021-07-18 17:49:34 +0300

    Create repository with start point

---
+++ Flappy Bird/Flappy Bird/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Flappy_Bird {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.MainMenuPanel = new System.Windows.Forms.Panel();
+            this.mainMenuGround = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.gameNameLabel = new System.Windows.Forms.Label();
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MainMenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainMenuGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // MainMenuPanel
+            // 
+            this.MainMenuPanel.BackColor = System.Drawing.Color.PaleGreen;
+            this.MainMenuPanel.Controls.Add(this.panel1);
+            this.MainMenuPanel.Controls.Add(this.startGameButton);
+            this.MainMenuPanel.Controls.Add(this.mainMenuGround);
+            this.MainMenuPanel.Controls.Add(this.pipeTop);
+            this.MainMenuPanel.Controls.Add(this.pipeBottom);
+            this.MainMenuPanel.Controls.Add(this.gameNameLabel);
+            this.MainMenuPanel.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.MainMenuPanel.Location = new System.Drawing.Point(0, 1);
+            this.MainMenuPanel.Name = "MainMenuPanel";
+            this.MainMenuPanel.Size = new System.Drawing.Size(531, 578);
+            this.MainMenuPanel.TabIndex = 0;
+            // 
+            // mainMenuGround
+            // 
+            this.mainMenuGround.Image = global::Flappy_Bird.Properties.Resources.ground;
+            this.mainMenuGround.Location = new System.Drawing.Point(0, 477);
+            this.mainMenuGround.Name = "mainMenuGround";
+            this.mainMenuGround.Size = new System.Drawing.Size(332, 101);
+            this.mainMenuGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainMenuGround.TabIndex = 0;
+            this.mainMenuGround.TabStop = false;
+            this.mainMenuGround.Visible = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::Flappy_Bird.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(327, -112);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(73, 253);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 2;
+            this.pipeTop.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::Flappy_Bird.Properties.Resources.pipe;
+            this.pipeBottom.Location = new System.Drawing.Point(327, 330);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(73, 248);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 1;
+            this.pipeBottom.TabStop = false;
+            // 
+            // gameNameLabel
+            // 
+            this.gameNameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameNameLabel.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameNameLabel.ForeColor = System.Drawing.Color.Green;
+            this.gameNameLabel.Location = new System.Drawing.Point(0, 0);
+            this.gameNameLabel.Name = "gameNameLabel";
+            this.gameNameLabel.Size = new System.Drawing.Size(531, 578);
+            this.gameNameLabel.TabIndex = 3;
+            this.gameNameLabel.Text = "Flappy Bird";
+            this.gameNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.startGameButton.FlatAppearance.BorderColor = System.Drawing.Color.PaleGoldenrod;
+            this.startGameButton.FlatAppearance.BorderSize = 0;
+            this.startGameButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.startGameButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.startGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startGameButton.Location = new System.Drawing.Point(188, 349);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.startGameButton.Size = new System.Drawing.Size(156, 92);
+            this.startGameButton.TabIndex = 4;
+            this.startGameButton.Text = "▶";
+            this.startGameButton.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel1.Location = new System.Drawing.Point(0, -7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 585);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(531, 577);
+            this.Controls.Add(this.MainMenuPanel);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyUp);
+            this.MainMenuPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainMenuGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Panel MainMenuPanel;
+        private System.Windows.Forms.PictureBox mainMenuGround;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.Label gameNameLabel;
+        private System.Windows.Forms.Button startGameButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Flappy Bird/Flappy Bird/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird {
+    public partial class Form1 : Form {
+        private int SceneSpeed = 3;
+        private int gravity = 5;
+        private int score = 0;
+        private Queue<PictureBox> groundClones = new Queue<PictureBox>();
+        public Form1() {
+            InitializeComponent();
+            InitMenuGround();
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e) {
+            /*  flappyBird.Top += gravity;
+              pipeBottom.Left -= pipeSpeed;
+              pipeTop.Left -= pipeSpeed;
+
+              if (flappyBird.Bounds.IntersectsWith(pipeBottom.Bounds) ||
+                  flappyBird.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                  flappyBird.Bounds.IntersectsWith(ground.Bounds)) {
+                  EndGame();
+              }
+              if (pipeBottom.Left < -100) {
+                  pipeBottom.Left = 550;
+                  pipeTop.Left = 550;
+                  score++;
+                  scoreValue.Text = "Score: " + score;
+              }*/
+
+            MainMenuGroundMove();
+
+        }
+
+        private void MainMenuGroundMove() {
+            foreach (PictureBox pic in groundClones) {
+                pic.Left -= SceneSpeed;
+            }
+
+            pipeBottom.Left -= SceneSpeed;
+            pipeTop.Left -= SceneSpeed;
+
+            if (pipeTop.Left < -pipeTop.Width) {
+                pipeBottom.Left = MainMenuPanel.Width + pipeTop.Width;
+                pipeTop.Left = MainMenuPanel.Width + pipeTop.Width;
+            }
+
+            if ((groundClones.Peek()).Left < -mainMenuGround.Width) {
+                PictureBox tmp = groundClones.Dequeue();
+                tmp.Left += 3*tmp.Width-10;
+                groundClones.Enqueue(tmp);
+            }
+        }
+
+        private void InitMenuGround() {
+            for (int i = 0; i <= 2; ++i) {
+                PictureBox tmp = new PictureBox();
+                tmp.Image = global::Flappy_Bird.Properties.Resources.ground;
+                
+                if(i == 0) tmp.Location = new System.Drawing.Point(0, 476);
+                else if (i == 1) tmp.Location = new System.Drawing.Point(mainMenuGround.Width, 476);
+                else tmp.Location = new System.Drawing.Point(2 * mainMenuGround.Width, 476);
+                
+                tmp.Size = new Size(337, 101);
+                tmp.SizeMode = PictureBoxSizeMode.StretchImage;
+                groundClones.Enqueue(tmp);
+                MainMenuPanel.Controls.Add(tmp);
+            }
+
+            pipeTop.SendToBack();
+            pipeBottom.SendToBack();
+            gameNameLabel.SendToBack();
+        }
+
+        private void gameKeyDown(object sender, KeyEventArgs e) {
+           // if (e.KeyCode == Keys.Space) {
+                //gravity -= 10;
+          //  }
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e) {
+           // if (e.KeyCode == Keys.Space) {
+           //     gravity = 5;
+           // }
+        }
+
+        private void EndGame() {
+            //gameTimer.Stop();
+           // gameOverPanel.Visible = true;
+        }
+
+        private void gameOverScore_Click(object sender, EventArgs e) {
+
+        }
+
+        private void continueButton_Click(object sender, EventArgs e) {
+           // gameTimer.Start();
+           // gameOverPanel.Visible = false;
+        }
+    }
+}
